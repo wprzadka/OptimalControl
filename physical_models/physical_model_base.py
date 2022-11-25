787debe678cbddcfe92395c7abec8fe6850a1e0e
wprzadka <wiktor.przadka.1@gmail.com>
2022-11-25 08:05:23 +0100

    add linear system update procedure implementation to base class

--- physical_models/physical_model_base.py
+++ physical_models/physical_model_base.py
@@ -3,8 +3,14 @@ import numpy as np
 
 class PhysicalModelBase:
 
+    state = None
+    A_mat = None
+    B_mat = None
+
     def update(self, action: np.ndarray, dt: float) -> np.ndarray:
-        pass
+        state_derivative = self.A_mat @ self.state + self.B_mat @ action
+        self.state += state_derivative * dt
+        return self.state
 
     def render(self, window):
         pass
