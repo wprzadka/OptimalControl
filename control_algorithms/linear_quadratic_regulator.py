774be1f410ff712683ef5e7f9a38024143fd8e8d
wprzadka <wiktor.przadka.1@gmail.com>
2022-12-02 12:21:48 +0100

    make LQR work properly

--- control_algorithms/linear_quadratic_regulator.py
+++ control_algorithms/linear_quadratic_regulator.py
@@ -6,14 +6,15 @@ from .costs_weights import CostWeights
 
 class LinearQuadraticRegulator:
 
-    def __init__(self, model: PhysicalModelBase, cost_weights: CostWeights, time_horizon: int):
+    def __init__(self, model: PhysicalModelBase, cost_weights: CostWeights, time_horizon: float):
 
         self.model = model
         self.time_horizon = time_horizon
 
+        self.cost_weights = cost_weights
         self.target_state = np.zeros_like(model.state)
         self.state_cost = cost_weights.state_cost * np.eye(model.state.shape[0])
-        self.control_cost = cost_weights.control_cost * np.eye(model.state.shape[0])
+        self.control_cost = cost_weights.control_cost * np.eye(model.B().shape[1])
         # self.control_cost = np.diag(np.full_like(model.state, fill_value=cost_weights.control_cost))
         self.end_state_cost = cost_weights.end_state_cost * np.eye(model.state.shape[0])
         # self.end_state_cost = np.diag(np.full_like(model.state, fill_value=cost_weights.end_state_cost))
@@ -26,20 +27,23 @@ class LinearQuadraticRegulator:
 
     def compute_action(self, state:np.ndarray, dt: float):
         K_vals = self.compute_riccati(dt)
-        print([i for i, v in enumerate(K_vals) if not np.any(np.isnan(v))])
 
-        # for t, k_val in enumerate(K_vals):
-        #     u
-
-        u_star = self.model.B().T @ K_vals[0] @ state
-        print(u_star)
+        inv_control_cost = np.linalg.inv(self.control_cost)
+        u_star = inv_control_cost @ self.model.B().T @ K_vals[0] @ (state - self.target_state)
         return u_star
 
     def compute_riccati(self, dt: float):
         K_vals = np.empty(shape=(int(np.ceil(self.time_horizon / dt)), *self.end_state_cost.shape))
         K_vals[-1] = -self.end_state_cost
         B = self.model.B()
+        A = self.model.A()
+        inv_control_cost = np.linalg.inv(self.control_cost)
 
         for t in range(len(K_vals) - 1, 0, -1):
-            K_vals[t - 1] = K_vals[t] + dt * (K_vals[t] @ B @ B.T @ K_vals[t] + self.state_cost)
+            K_vals[t - 1] = K_vals[t] + dt * (
+                    K_vals[t] @ B @ inv_control_cost @ B.T @ K_vals[t]
+                    + K_vals[t] @ A
+                    + A.T @ K_vals[t]
+                    - self.state_cost
+            )
         return K_vals
--- main.py
+++ main.py
@@ -11,12 +11,13 @@ from physical_models.seeker import Seeker
 if __name__ == '__main__':
 
     # model = InvertedPendulum()
-    model = Seeker(velocity=50.)
-    # model = RocketRailroadCar(velocity=50.)
+    # model = Seeker(velocity=50.)
+    model = RocketRailroadCar(velocity=1.)
 
-    control = None
+    # control = None
     # control = PID(model.state.shape[0], 1, 0, 0)
-    # control = LinearQuadraticRegulator(model=model, cost_weights=CostWeights(1, 1, 0), time_horizon=2)
+    control = LinearQuadraticRegulator(model=model, cost_weights=CostWeights(1, 1, 0), time_horizon=0.5)
+    control.set_target(np.array([500, 0]))
 
     sim = Simulation(model, control)
 
