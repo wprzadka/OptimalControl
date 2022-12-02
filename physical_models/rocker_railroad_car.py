682fa67d9596ab1cf34c60f6e4ed1ca5b91c2b4a
wprzadka <wiktor.przadka.1@gmail.com>
2022-12-02 10:12:35 +0100

    make simple railroad car model for LQR tests

---
+++ physical_models/rocker_railroad_car.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pygame as pg
+
+from physical_models.physical_model_base import PhysicalModelBase
+
+
+class RocketRailroadCar(PhysicalModelBase):
+
+    def __init__(self, velocity):
+        self.state = np.array([100., 0.])
+        self.velocity = velocity
+
+        self.A_mat = np.array([
+            # x v
+            [0., 1.],  # x
+            [0., 0.]  # v
+        ])
+        self.B_mat = np.array([
+            # a
+            [0], # x
+            [self.velocity]  # v
+        ])
+        self.action_space = np.array([[-1., 1.]])
+
+
+    def A(self):
+        return self.A_mat
+
+    def B(self):
+        return self.B_mat
+
+    def render(self, window):
+        y = 200
+        size = (50, 20)
+        x, v = self.state
+        center = np.array([x, y])
+
+        body = pg.Rect(center - np.array([size[0] // 2, size[1] // 2]), size)
+        pg.draw.rect(window, color=(50, 120, 50), rect=body)
+
+        flame = center + np.sign(v) * np.array([[1, 0], [-1, -1], [-0.7, 0], [-1, 1]]) * 5
+        pg.draw.polygon(window, color=(255, 20, 50), points=flame)
+
+        pg.draw.circle(window, center=np.array([500, 200]), radius=8, color=(0, 50, 160))
+
+
+
+    def get_input(self):
+        pressed = pg.key.get_pressed()
+        action = np.array([0])
+
+        # get rotation speed
+        if pressed[pg.K_LEFT]:
+            action[0] = -1.
+        elif pressed[pg.K_RIGHT]:
+            action[0] = 1.
+
+        return action
